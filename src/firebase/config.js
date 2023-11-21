819af02258079ea1848d906e43af2780ad834fe3
Anais Moutarlier <anaismoutarlier@gmail.com>
2023-11-21 20:05:24 +0100

    add template env file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDiu4jzPOFU7UX46f4mcmiRJmYNK9bXD90",
-  authDomain: "minislack-8faa0.firebaseapp.com",
-  projectId: "minislack-8faa0",
-  storageBucket: "minislack-8faa0.appspot.com",
-  messagingSenderId: "548468208807",
-  appId: "1:548468208807:web:a63838b965c043b1535782",
+  apiKey: process.env.FB_APIKEY,
+  authDomain: process.env.FB_AUTHDOMAIN,
+  projectId: process.env.FB_PROJECTID,
+  storageBucket: process.env.FB_MESSAGINGSENDERID,
+  messagingSenderId: process.env.FB_MESSAGINGSENDERID,
+  appId: process.env.FB_APPID,
 };
